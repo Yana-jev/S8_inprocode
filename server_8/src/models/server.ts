e36566acf35088ee6f5d2ecbeee6fd9d1e3309c8
Yana-jev <evko.yana@gmail.com>
2024-09-09 12:41:32 +0200

    created server

---
+++ server_8/src/models/server.ts
@@ -0,0 +1,57 @@
+import express, {Application, Request, Response} from 'express';
+import cors from 'cors'
+import routesProducto from '../routes/producto';
+import db from '../db/connection';
+
+class Server {
+
+   private app: Application;
+   private port: string;
+
+   constructor(){
+      
+      this.app = express();
+      this.port = process.env.PORT || '3001';
+      this.listen();
+      this.midlewares();
+      this.routes();
+      this.dbConnect();
+   }
+
+
+   listen(){
+      this.app.listen(this.port, ()=>{
+         console.log(`Aplicacion corriendo en el puerto ${this.port}`)
+      })
+   }
+
+
+   routes(){
+      this.app.get('/', (req: Request, res: Response)=>{
+         res.json({
+            msg: 'api working'
+         })
+
+      })
+      this.app.use('/api/productos', routesProducto)
+   }
+
+   midlewares(){
+      this.app.use(express.json());
+
+      //cors
+      this.app.use(cors());
+   }
+
+   async dbConnect(){
+
+      try {
+         await db.authenticate();
+         console.log('Base de datos conectada');
+   } catch (error) {
+         console.error('error al conectarse a base de datos:', error);
+   }
+   }
+}
+
+export default Server;
